cbb96782eafe38d1a37490c838cd059315fa5bcb
Luis Pedro Coelho <lpc@cmu.edu>
2011-01-08 19:08:11 -0500

    TST: Use unified diffs

--- hex/tests.zsh
+++ hex/tests.zsh
@@ -2,9 +2,9 @@
 
 make --quiet
 ./hextest
-for sub in `ls tests`; do 
+for sub in `ls tests`; do
     for t in tests/$sub/*.hex; do
-        ./hex $sub $t | diff - tests/$sub/`basename $t hex`output 
+        ./hex $sub $t | diff -u - tests/$sub/`basename $t hex`output
     done
 done
 
