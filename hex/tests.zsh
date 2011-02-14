8d024c41caa877af75c6ad5f92253a8cef13f117
Luis Pedro Coelho <lpc@cmu.edu>
2011-02-13 21:25:28 -0500

    TST Add -v flag (for verbose)

--- hex/tests.zsh
+++ hex/tests.zsh
@@ -4,6 +4,9 @@ make --quiet
 ./hextest
 for sub in `ls tests`; do
     for t in tests/$sub/*.hex; do
+        if test "$1" = "-v"; then
+            echo testing $sub $t ...
+        fi
         ./hex $sub $t | diff -u - tests/$sub/`basename $t hex`output
     done
 done
