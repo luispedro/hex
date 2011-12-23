309140dabc3e10208cfaed8a57f340d7f313ee52
Luis Pedro Coelho <luis@luispedro.org>
2011-12-23 22:03:58 +0100

    TST Test DVI output
    This testing method is very stringent, but will catch mistakes

--- hex/tests.zsh
+++ hex/tests.zsh
@@ -3,11 +3,20 @@
 make --quiet
 ./hextest
 for sub in `ls tests`; do
+    if test $sub = "dvidecode"; then
+        function run() {
+            ./hex $1 | ./dvidecode
+        }
+    else
+        function run() {
+            ./hex --mode=$sub $1
+        }
+    fi
     for t in tests/$sub/*.hex; do
         if test "$1" = "-v"; then
             echo testing $sub $t ...
         fi
-        ./hex --mode=$sub $t | diff -u - tests/$sub/`basename $t hex`output
+        run $t | diff -u - tests/$sub/`basename $t hex`output
     done
 done
 
